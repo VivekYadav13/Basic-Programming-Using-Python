24db363669ce6e224698c751ee40e89e99537e76
VivekYadav13 <128254785+VivekYadav13@users.noreply.github.com>
2023-04-16 00:39:06 +0530

    Create problem_6.py

---
+++ Practice sheet 4/problem_6.py
@@ -0,0 +1,17 @@
+'''Write a function that takes an integer n and returns a random integer with exactly n digits. For
+instance, if n is 3, then 125 and 593 would be valid return values, but 093 would not because
+that is really 93, which is a two-digit number.'''
+from random import randint
+def ran(n):
+    s=''
+    while n>0:
+        if n==1:
+            s+=str(randint(1,9))
+        else:
+            s+=str(randint(0,9))
+        n-=1
+    return s
+a=int(input('Enter number of digits : '))
+print(int(ran(a)))
+
+        

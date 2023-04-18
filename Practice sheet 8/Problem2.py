1e91e388113a50496443369704378a7c20818fdb
VivekYadav13 <128254785+VivekYadav13@users.noreply.github.com>
2023-04-18 17:01:53 +0530

    Create Problem2.py

---
+++ Practice sheet 8/Problem2.py
@@ -0,0 +1,4 @@
+'''Create a list of random numbers and remove all duplicate elements in the list.'''
+a=list(input('Enter numbers in the list: '))
+c=set(a)
+print('Duplicates number have been removed from the list :',c)
